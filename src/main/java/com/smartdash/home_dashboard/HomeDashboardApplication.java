5746182569ecc2c5a017357043a2987aebd009fa
Danny Zamora <dannyzamora.js@gmail.com>
2025-06-29 22:04:06 -0700

    Initial commit

---
+++ src/main/java/com/smartdash/home_dashboard/HomeDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.smartdash.home_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartdash/home_dashboard/HomeDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartdash.home_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
