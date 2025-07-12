357ed8b123ce05124f922c9ab781567acfed9ac3
Danny Zamora <dannyzamora.js@gmail.com>
2025-07-12 00:09:49 -0700

    need to push this branch

--- src/main/java/com/smartdash/homedashboard/controller/TestController.java
+++ src/main/java/com/smartdash/homedashboard/controller/TestController.java
@@ -1,14 +1,19 @@
 package com.smartdash.homedashboard.controller;
 
+import java.util.Date;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/test")
 public class TestController {
 
     // This is a test controller to verify the application is running
-    @GetMapping("/test")
-    public String testEndpoint() {
-        return "Test endpoint is working!";
-    }
+@GetMapping("/time")
+public ResponseEntity<Date> testEndpoint() {
+    return ResponseEntity.ok(new Date());  // 200 OK with body
+}
 }
